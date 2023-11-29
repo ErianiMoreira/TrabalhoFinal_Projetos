e43b621c81f3332b8d118d6f12c49ccb822618d3
ErianiMoreira <ems17720045@gmail.com>
2023-11-28 22:17:40 -0300

    Arquivos Iniciais

---
+++ gerenciamentoAcessos/src/main/java/GerenciamentoAcessos.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author Eriani M
+ */
+public class GerenciamentoAcessos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
